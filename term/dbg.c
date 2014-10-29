17acdb3120208e7ea908a43e19ea3b3faf43ccfa
Acorn Pooley <acorn@mad.scientist.com>
2014-10-29 02:24:41 -0700

    dbg: use select

--- term/dbg.c
+++ term/dbg.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -7,7 +8,24 @@
 #include <errno.h>
 #include <string.h>
 #include <ctype.h>
+#include <sys/select.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
 
+#define MIN(a,b) ((a)<(b)?(a):(b))
+#define MAX(a,b) ((a)>(b)?(a):(b))
+
+typedef struct SerialPort
+{
+  int fd;
+  struct SerialPort *next;
+  char *dev;
+  int bufcnt;
+  char buf[200];
+} SerialPort;
+
+SerialPort *g_serial_ports = NULL;
 
 void showTermios(const struct termios *tio)
 {
@@ -87,13 +105,20 @@ void showTermios(const struct termios *tio)
 
 int openDeviceFile(const char *dev)
 {
-  int fd = open(dev, O_RDWR | O_CLOEXEC | O_NOCTTY | O_NONBLOCK);
+  int fd;
   struct termios tio;
   int rv;
 
+  if (!dev)
+    return -1;
+
+  errno = 0;
+  fd = open(dev, O_RDWR | O_CLOEXEC | O_NOCTTY | O_NONBLOCK);
   if (fd < 0)
   {
-    fprintf(stderr, "Failed to open device '%s'\n",dev);
+    fprintf(stderr, "Failed to open device '%s'.  %s\n",
+      dev,
+      strerror(errno));
     return -1;
   }
 
@@ -143,35 +168,182 @@ int openDeviceFile(const char *dev)
   return fd;
 }
 
-int main(int argc, char *argv[])
+void addSerialPort(const char *device_file)
 {
-  int fd = openDeviceFile("/dev/ttyACM1");
+  SerialPort *sp = calloc(1, sizeof(SerialPort));
+  if (!sp)
+    return;
 
-  for (;;)
+  sp->dev = strdup(device_file);
+  sp->fd = openDeviceFile(sp->dev);
+  if (sp->fd < 0)
   {
-    char buf[1];
-    int n;
-    errno = 0;
-    n = read(fd, buf, 1);
-
-    if (n > 0)
-    {
-      printf("GOT: %3d = 0x%02x '%c'\n",
-        buf[0],
-        buf[0],
-        isprint(buf[0]) ? buf[0] : '?');
-    }
-    else if (n != 0)
-    {
-      fprintf(stderr, "read failed.  rv=%d  errno=%d = %s\n",
-        n,
-        errno,
-        strerror(errno));
-    }
+    free(sp->dev);
+    free(sp);
+    return;
   }
 
-  if (fd >= 0)
-    close(fd);
+  sp->next = g_serial_ports;
+  g_serial_ports = sp;
+}
+
+void removeSerialPort(const char *device_file)
+{
+  SerialPort **pp = &g_serial_ports;
+
+  while(*pp)
+  {
+    if (!strcmp((*pp)->dev, device_file))
+    {
+      SerialPort *next = (*pp)->next;
+      close((*pp)->fd);
+      free((*pp)->dev);
+      free(*pp);
+      *pp = next;
+    }
+    else
+    {
+      pp = &(*pp)->next;
+    }
+  }
+}
+
+int hexDigit(int nibble)
+{
+  nibble &= 0xf;
+  if (nibble <= 9)
+    return '0' + nibble;
+  return 'a' + nibble - 10;
+}
+
+void doRead(SerialPort *sp)
+{
+  int gotcr = 0;
+  int n;
+  errno = 0;
+  n = read(sp->fd, sp->buf + sp->bufcnt, sizeof(sp->buf) - sp->bufcnt - 1);
+
+  if (n > 0)
+  {
+    int j = 0;
+    char buf[250];
+    sp->bufcnt += n;
+
+    for (n = 0 ; n < sp->bufcnt ; ++n)
+    {
+      int c = sp->buf[n];
+      if (c == '\n')
+      {
+        gotcr = 0;
+      }
+      else if (c == '\r')
+      {
+        gotcr = 1;
+      }
+      else
+      {
+        if (gotcr)
+        {
+          buf[j++] = '\\';
+          buf[j++] = 'x';
+          buf[j++] = '0';
+          buf[j++] = 'd';
+          gotcr = 0;
+        }
+        if (sp->buf[n] < 0x20 || sp->buf[n] > 0x7e)
+        {
+          buf[j++] = '\\';
+          buf[j++] = 'x';
+          buf[j++] = hexDigit(c >> 4);
+          buf[j++] = hexDigit(c & 0xf);
+        }
+        else
+        {
+          buf[j++] = c;
+        }
+      }
+
+      if (c == '\n' || j > sizeof(buf) - 5)
+      {
+        buf[j++] = 0;
+        printf("%s: %s\n",
+          sp->dev,
+          buf);
+
+        memmove(sp->buf, sp->buf + n + 1, sp->bufcnt - n - 1);
+        sp->bufcnt -= n + 1;
+        n = -1;
+      }
+    }
+  }
+  else if (n == 0)
+  {
+    return;
+  }
+  else
+  {
+    fprintf(stderr, "Error in read(%s).  %s\n",
+      sp->dev,
+      strerror(errno));
+  }
+}
+
+void doSelect(void)
+{
+  fd_set fdr, fdw, fdx;
+  int n;
+  int cnt = 0;
+  SerialPort *sp;
+
+  FD_ZERO(&fdr);
+  FD_ZERO(&fdw);
+  FD_ZERO(&fdx);
+
+  for (sp = g_serial_ports ; sp ; sp = sp->next)
+  {
+    cnt = MAX(cnt, sp->fd + 1);
+    FD_SET(sp->fd, &fdr);
+  }
+
+  errno = 0;
+  n = pselect(cnt, &fdr, &fdw, &fdx, NULL, NULL);
+
+  if (n > 0)
+  {
+    for (sp = g_serial_ports ; sp ; sp = sp->next)
+    {
+      if (FD_ISSET(sp->fd, &fdr))
+      {
+        doRead(sp);
+      }
+    }
+  }
+  else if (n == 0)
+  {
+    printf("pselect returned 0\n");
+  }
+  else
+  {
+    fprintf(stderr, "Error in pselect.  %s\n",
+      strerror(errno));
+  }
+}
+
+void run()
+{
+  for(;;)
+  {
+    doSelect();
+  }
+}
+
+int main(int argc, char *argv[])
+{
+  addSerialPort("/dev/ttyACM1");
+
+  run();
+
+  removeSerialPort("/dev/ttyACM1");
 
   return 0;
 }
