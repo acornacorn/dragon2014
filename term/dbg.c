dc14a17670dec2881ff31bc1026f3d9ea5bdeea9
Acorn Pooley <acorn@mad.scientist.com>
2014-10-29 01:39:03 -0700

    reading serial works

--- term/dbg.c
+++ term/dbg.c
@@ -4,15 +4,24 @@
 #include <fcntl.h>
 #include <termios.h>
 #include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <ctype.h>
 
 
 void showTermios(const struct termios *tio)
 {
+  speed_t ispeed = cfgetispeed(tio);
+  speed_t ospeed = cfgetospeed(tio);
 
-  printf("c_iflag = 0x%08x\n", tio->c_iflag);
-  printf("c_oflag = 0x%08x\n", tio->c_oflag);
-  printf("c_cflag = 0x%08x\n", tio->c_cflag);
-  printf("c_lflag = 0x%08x\n", tio->c_lflag);
+  printf("got termios\n");
+  printf("  c_iflag = 0x%08x\n", tio->c_iflag);
+  printf("  c_oflag = 0x%08x\n", tio->c_oflag);
+  printf("  c_cflag = 0x%08x\n", tio->c_cflag);
+  printf("  c_lflag = 0x%08x\n", tio->c_lflag);
+
+  printf("  ispeed = %ld\n",(long)ispeed);
+  printf("  ospeed = %ld\n",(long)ospeed);
 
   {
     int i;
@@ -20,7 +29,7 @@ void showTermios(const struct termios *tio)
     {
       if (tio->c_cc[i] != 0)
       {
-        printf("  c_cc[0x%03x] = %3d = %02x\n",
+        printf("    c_cc[0x%03x] = %3d = %02x\n",
           i,
           tio->c_cc[i],
           tio->c_cc[i]);
@@ -29,16 +38,19 @@ void showTermios(const struct termios *tio)
   }
 }
 
-// Arduino sets ttyACM* as follows:
-//  c_iflag = 0x00000010
-//  c_oflag = 0x00000000
-//  c_cflag = 0x000008bd
-//  c_lflag = 0x00000000
+
+// Default termios for ttyACM* device file:
+//  c_iflag = 00000001
+//  c_oflag = 00000000
+//  c_cflag = 00000cbd
+//  c_lflag = 00000000
 //    c_cc[0x000] =   3 = 03
 //    c_cc[0x001] =  28 = 1c
 //    c_cc[0x002] = 127 = 7f
 //    c_cc[0x003] =  21 = 15
 //    c_cc[0x004] =   4 = 04
+//    c_cc[0x005] =   5 = 05
+//    c_cc[0x006] =   1 = 01
 //    c_cc[0x008] =  17 = 11
 //    c_cc[0x009] =  19 = 13
 //    c_cc[0x00a] =  26 = 1a
@@ -48,11 +60,35 @@ void showTermios(const struct termios *tio)
 //    c_cc[0x00f] =  22 = 16
 
 
+// Arduino sets ttyACM* as follows:
+//  c_iflag = 0x00000010           -IGNBRK +INPCK
+//  c_oflag = 0x00000000
+//  c_cflag = 0x000008bd           -CREAD
+//  c_lflag = 0x00000000
+//  ispeed = B9600
+//  ospeed = B9600
+//    c_cc[0x000] =   3 = 03  VINTR
+//    c_cc[0x001] =  28 = 1c  VQUIT
+//    c_cc[0x002] = 127 = 7f  VERASE
+//    c_cc[0x003] =  21 = 15  VKILL
+//    c_cc[0x004] =   4 = 04  VEOF
+//    c_cc[0x005] =   0 = 00  VTIME     default: 5
+//    c_cc[0x006] =   0 = 00  VMIN      default: 1
+//    c_cc[0x007] =   0 = 00  VSWTC
+//    c_cc[0x008] =  17 = 11  VSTART
+//    c_cc[0x009] =  19 = 13  VSTOP
+//    c_cc[0x00a] =  26 = 1a  VSUSP
+//    c_cc[0x00c] =  18 = 12  VREPRINT
+//    c_cc[0x00d] =  15 = 0f  VDISCARD
+//    c_cc[0x00e] =  23 = 17  VWERASE
+//    c_cc[0x00f] =  22 = 16  VLNEXT
 
-int setup(const char *dev)
+
+
+int openDeviceFile(const char *dev)
 {
   int fd = open(dev, O_RDWR | O_CLOEXEC | O_NOCTTY | O_NONBLOCK);
-  struct termios tio_orig;
+  struct termios tio;
   int rv;
 
   if (fd < 0)
@@ -63,17 +99,76 @@ int setup(const char *dev)
 
   printf("Opened device\n");
 
-  rv = tcgetattr(fd, &tio_orig);
+  errno = 0;
+  rv = tcgetattr(fd, &tio);
+  if (rv)
+  {
+    fprintf(stderr, "tcgetattr failed. %s\n",
+      strerror(errno));
+    close(fd);
+    return -1;
+  }
 
-  printf("got termios  rv=%d\n",rv);
-  showTermios(&tio_orig);
+  showTermios(&tio);
+
+  tio.c_iflag &= ~IGNBRK;
+  tio.c_iflag |= INPCK;
+
+  tio.c_cflag &= ~CREAD;
+
+  #define FORCE_TERMIO_FLAGS 1
+  if (FORCE_TERMIO_FLAGS)
+  {
+    tio.c_iflag = INPCK;
+    tio.c_oflag = 0;
+    tio.c_cflag = 0x8bd;
+    tio.c_lflag = 0;
+  }
+
+  tio.c_cc[VTIME] = 0;
+  tio.c_cc[VMIN] = 0;
+
+  cfsetispeed(&tio, B9600);
+  cfsetospeed(&tio, B9600);
+
+  errno = 0;
+  rv = tcsetattr(fd, TCSAFLUSH, &tio);
+
+  if (rv)
+  {
+    fprintf(stderr, "tcsetattr failed.  %s\n",
+      strerror(errno));
+  }
 
   return fd;
 }
 
 int main(int argc, char *argv[])
 {
-  int fd = setup("/dev/ttyACM1");
+  int fd = openDeviceFile("/dev/ttyACM1");
+
+  for (;;)
+  {
+    char buf[1];
+    int n;
+    errno = 0;
+    n = read(fd, buf, 1);
+
+    if (n > 0)
+    {
+      printf("GOT: %3d = 0x%02x '%c'\n",
+        buf[0],
+        buf[0],
+        isprint(buf[0]) ? buf[0] : '?');
+    }
+    else if (n != 0)
+    {
+      fprintf(stderr, "read failed.  rv=%d  errno=%d = %s\n",
+        n,
+        errno,
+        strerror(errno));
+    }
+  }
 
   if (fd >= 0)
     close(fd);
