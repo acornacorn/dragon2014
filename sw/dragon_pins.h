682978cb77cd88b9162eba104724e02d608e9d0b
Acorn Pooley <acorn@mad.scientist.com>
2014-10-31 00:14:35 -0700

    set servo ranges and colors (from experiments)

--- sw/dragon_pins.h
+++ sw/dragon_pins.h
@@ -10,23 +10,60 @@ static const int dpin_leye_servo = 15;
 static const int dpin_reye_servo = 16;
 static const int dpin_lips_servo = 17;
 
-static const int sv_look_min = 29;
-static const int sv_look_mid = 107;
-static const int sv_look_max = 176;
+//================ LOOK
 
-static const int sv_leye_min = 29;
-static const int sv_leye_mid = 107;
-static const int sv_leye_max = 176;
-static const int sv_leye_closed = 107;
+static const int sv_look_min = 68;  // right (burst only)
+static const int sv_look_mid = 93;
+static const int sv_look_max = 120; // left (burst only)
+// max/min steady state
+static const int sv_look_min_ss = 82;  // right
+static const int sv_look_max_ss = 110; // left
 
-static const int sv_reye_min = 29;
-static const int sv_reye_mid = 107;
-static const int sv_reye_max = 176;
-static const int sv_reye_closed = 107;
+static const int sv_look_mid_from_r = 97;
+static const int sv_look_mid_from_l = 89;
 
-static const int sv_lips_min = 29;
-static const int sv_lips_mid = 107;
-static const int sv_lips_max = 176;
+// back off by this amount to avoid jitter
+static const int sv_look_backoff = 4;
+
+//================ LEYE
+
+static const int sv_leye_min = 67; // open
+static const int sv_leye_mid = 101;
+static const int sv_leye_max = 168; // closed
+
+static const int sv_leye_wide_open = 67;
+static const int sv_leye_droopy = 81;
+static const int sv_leye_guarded = 101;
+static const int sv_leye_angry = 115;
+
+static const int sv_leye_closed = 168;
+
+//================ REYE
+
+static const int sv_reye_min = 71; // closed
+static const int sv_reye_mid = 139;
+static const int sv_reye_max = 171; // open
+
+static const int sv_reye_wide_open = 170;
+static const int sv_reye_droopy = 156;
+static const int sv_reye_guarded = 139;
+static const int sv_reye_angry = 123;
+
+static const int sv_reye_closed = 71;
+
+//================ LIPS
+
+static const int sv_lips_min = 59;    // open
+static const int sv_lips_mid = 160;
+static const int sv_lips_max = 178;  // closed
+
+static const int sv_lips_soft_max = 162;
+static const int sv_lips_back_teeth_closed = 162;
+static const int sv_lips_back_teeth_open = 136;
+
+static const int sv_lips_nose_closed = 136;
+static const int sv_lips_nose_open = 63;
+
 
 
 
@@ -37,6 +74,21 @@ static const int dpin_eye_green = 22;
 static const int apin_eye_green = 8;
 static const int dpin_eye_blue = 23;
 static const int apin_eye_blue = 9;
+
+
+// colors C(R,G,B)  RGB are 0...255
+#define COLOR_ANGRY_RED         C(101,0,0)
+#define COLOR_ICE               C(207,0,236)
+#define COLOR_MAGENTA           C(207,0,22)
+#define COLOR_ICE1              C(112,0,200)
+#define COLOR_ICE_BLUE          C(44,0,200)
+#define COLOR_ICE_VERY_BLUE     C(0,0,200)
+#define COLOR_GREEN             C(0,200,0)
+#define COLOR_GOLD              C(255,119,0)
+#define COLOR_GOLD_ORANGE       C(255,42,0)
+#define COLOR_SICKLY            C(255,219,0)
+#define COLOR_WHITE             C(255,219,103)
+
 
 // buttons
 static const int dpin_button0 = 12;
