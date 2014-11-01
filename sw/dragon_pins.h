de17c734e074cbc288e4eeb121c652381507ef13
Acorn Pooley <acorn@mad.scientist.com>
2014-10-31 18:07:55 -0700

    tweak servo values

--- sw/dragon_pins.h
+++ sw/dragon_pins.h
@@ -16,14 +16,14 @@ static const int sv_look_min = 68;  // right (burst only)
 static const int sv_look_mid = 93;
 static const int sv_look_max = 120; // left (burst only)
 // max/min steady state
-static const int sv_look_min_ss = 82;  // right
-static const int sv_look_max_ss = 110; // left
+static const int sv_look_min_ss = 70;  // right
+static const int sv_look_max_ss = 115; // left
 
-static const int sv_look_mid_from_r = 97;
-static const int sv_look_mid_from_l = 89;
+static const int sv_look_mid_from_r = 104; //97;
+static const int sv_look_mid_from_l = 94; //89;
 
-static const int sv_look_left = 110;
-static const int sv_look_right = 82;
+static const int sv_look_left = 115;
+static const int sv_look_right = 70;
 
 // back off by this amount to avoid jitter
 static const int sv_look_backoff = 4;
@@ -35,8 +35,8 @@ static const int sv_leye_mid = 101;
 static const int sv_leye_max = 168; // closed
 
 static const int sv_leye_wide_open = 67;
-static const int sv_leye_droopy = 81;
-static const int sv_leye_guarded = 101;
+static const int sv_leye_droopy = 84;
+static const int sv_leye_guarded = 99;
 static const int sv_leye_angry = 115;
 
 static const int sv_leye_closed = 168;
@@ -45,12 +45,12 @@ static const int sv_leye_closed = 168;
 
 static const int sv_reye_min = 71; // closed
 static const int sv_reye_mid = 139;
-static const int sv_reye_max = 171; // open
+static const int sv_reye_max = 165; // open
 
-static const int sv_reye_wide_open = 170;
-static const int sv_reye_droopy = 156;
-static const int sv_reye_guarded = 139;
-static const int sv_reye_angry = 123;
+static const int sv_reye_wide_open = 165;
+static const int sv_reye_droopy = 151;
+static const int sv_reye_guarded = 137;
+static const int sv_reye_angry = 120;
 
 static const int sv_reye_closed = 71;
 
